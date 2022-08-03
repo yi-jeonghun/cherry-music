848c9ff1b3315b7a2f0718ad6b5c943ff909ebae
Jeonghun Lee <yi.jeonghun@gmail.com>
2022-08-03 20:57:40 +0900

    home 화면 구현

--- trot/js/home_control.js
+++ trot/js/home_control.js
@@ -12,18 +12,70 @@ function HomeControl(){
 				location.reload();
 			}
 		});
+
+		self.LoadMusicListMulti();
+		self.LoadMusicListSingle();
+
 		return this;
 	};
 
-	this.Play = function(video_id){
-		console.log('[web] video_id ' + video_id);
-		var message = {
-			head: 'mango',
-			video_id: video_id,
-			title: '제목입니당',
-			artist: '나가수'
-		};
-		console.log('[web] message.video_id ' + message.video_id);
-		parent.postMessage(message, "*");
+	this.LoadMusicListMulti = function(){
+		$.getJSON( "db/music_multi_list.json", function(music_list) {
+			self.DISP_RecentMulti(music_list);
+		});
+	};
+
+	this.LoadMusicListSingle = function(){
+		$.getJSON( "db/music_single_list.json", function(music_list) {
+			self.DISP_RecentSingle(music_list);
+		});
+	};
+
+	this.DISP_RecentMulti = function(music_list){
+		var h = ``;
+		for(var i=0 ; i<music_list.length ; i++){
+			if(i > 9){
+				break;
+			}
+			var m = music_list[i];
+			var on_click_play = `SendMusicToPlayer('${m.music_uid}', '${m.title}', '${m.artist_uid}', '${m.artist}', '${m.video_id}')`;
+			h += `
+			<div style="padding: 5px;">
+				<div style="width: 200px; color: red; position:relative; cursor:pointer" onClick="${on_click_play}">
+					<img style="width: 100%; height: auto;" src="https://img.youtube.com/vi/${m.video_id}/0.jpg">
+					<div style="position: absolute; left:90px; top:60px">
+						<i id="id_btn_play_pause" class="fas fa-play" style="color:red; font-size: 2.5em;"></i>
+					</div>
+				</div>
+				<div style="margin-top: 5px">${m.title}</div>
+				<div>${m.artist}</div>
+			</div>
+			`;
+		}
+		$('#id_div_multi_list').html(h);
+	};
+
+	this.DISP_RecentSingle = function(music_list){
+		var h = ``;
+		for(var i=0 ; i<music_list.length ; i++){
+			if(i > 9){
+				break;
+			}
+			var m = music_list[i];
+			var on_click_play = `SendMusicToPlayer('${m.music_uid}', '${m.title}', '${m.artist_uid}', '${m.artist}', '${m.video_id}')`;
+			h += `
+			<div style="padding: 5px">
+				<div style="width: 200px; color: red; position:relative; cursor:pointer" onClick="${on_click_play}">
+					<img style="width: 100%; height: auto;" src="https://img.youtube.com/vi/${m.video_id}/0.jpg">
+					<div style="position: absolute; left:90px; top:60px">
+						<i id="id_btn_play_pause" class="fas fa-play" style="color:red; font-size: 2.5em;"></i>
+					</div>
+				</div>
+				<div style="margin-top: 5px">${m.title}</div>
+				<div>${m.artist}</div>
+			</div>
+			`;
+		}
+		$('#id_div_single_list').html(h);
 	};
 }
